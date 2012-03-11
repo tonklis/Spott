62e50908062a0bbaa86697f09be445407150d253
tonklis <benjamin.hernandez@in2teck.com>
2012-03-11 08:17:58 -0600

    Se agrego un mejor nombre al archivo que se sube. 	modified:   app/models/dfile.rb

--- app/models/dfile.rb
+++ app/models/dfile.rb
@@ -5,11 +5,11 @@ class Dfile < ActiveRecord::Base
 	def self.save_file(params)
 		
 		dfile = Dfile.new
-    dfile.name = params['name']
 		dfile.user_id = User.find_by_u_id(params['u_id']).id
 		dfile.place_id = Place.find_by_id(params['place_id']).id
 		
 		file = params['userfile']
+    dfile.name = params['name'] + file.original_name
 		dfile.description = params['description']
     dir_name = "public/data/#{dfile.place_id}"
 		if not FileTest::directory?(dir_name)
